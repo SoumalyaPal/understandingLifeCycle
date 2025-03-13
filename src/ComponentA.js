9ba218f9b90858e3ac5ea73f52841de721d2f9ef
SOUMALYA PAL <soumalyapal721@gmail.com>
2025-03-13 23:36:17 +0530

    updated

--- src/ComponentA.js
+++ src/ComponentA.js
@@ -1,28 +1,42 @@
 import React from "react";
+import ComponentB from "./ComponentB";
 
 class ComponentA extends React.Component{
     constructor(){
         super();
         this.state = {
-            name: "ComponentA"
+            name: "ComponentA",
+            data:[]
         }
         console.log("ComponentA Constructor")
     }
 
-    static getDerivedStateFromProps(){
+    static getDerivedStateFromProps(props,state){
         console.log("ComponentA getDerivedStateFromProp")
         return null; 
     }
 
     componentDidMount(){
         console.log("ComponentA componentDidMount")
+        fetch('https://jsonplaceholder.typicode.com/users/')
+            .then(response => response.json())
+            .then(data => this.setState({data}))
     }
 
 
     render(){
-        console.log("ComponentA render")
-        return(
-            <h1>{this.state.name}</h1>
+        return(<>
+        <h1>{this.state.name}</h1>
+        <ul>
+            {this.state.data.map((d)=>{
+                return(
+                    <li>
+                        {d.username}
+                    </li>
+                )
+            })}
+        </ul>
+        </>    
         )
     }
 }
